60577353c7e17a0565486dd72a7109b7ae588873
François GUILTAUX <123474146+Zeilor@users.noreply.github.com>
2024-06-28 01:51:35 +0200

    Derniers détails bannière et déplacement dans .CSS

--- templates/header.php
+++ templates/header.php
@@ -17,43 +17,8 @@ echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>";
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <!-- **** H E A D **** -->
-<?php if (valider("connecte","SESSION")): ?>
+<?php if (!valider("connecte","SESSION")): ?>
 
-<style>
-    .icons{
-        height: 40px;
-        fill: white;
-    }
-    #banniere{
-        position: fixed;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        background-color: #f1f1f1;
-        display : flex;
-        justify-content: space-evenly;
-        align-items: center;
-        background-color: darkslategray;
-        padding: 5px;
-    }
-    #stitre{
-        width : 50%;
-        color : white;
-    }
-
-    @media only screen and (max-width: 1000px) {
-        .icons{
-            height: 70px;
-        }
-        #banniere{
-            padding: 10px;
-        }
-        #stitre{
-            display: none;
-        }
-        
-    }
-</style>
 
 <head>	
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-15" />
@@ -72,7 +37,7 @@ echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>";
     <img src="ressources/logo.png" />
 </div>
 
-<div>
+<div id="navbar">
     <a href="index.php?view=trajets.create"><svg class="icons" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg></a>
     <a href="index.php?view=trajets.search"><svg class="icons" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg></a>
     <a href="index.php?view=accueil"><svg class="icons" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M135.2 117.4L109.1 192H402.9l-26.1-74.6C372.3 104.6 360.2 96 346.6 96H165.4c-13.6 0-25.7 8.6-30.2 21.4zM39.6 196.8L74.8 96.3C88.3 57.8 124.6 32 165.4 32H346.6c40.8 0 77.1 25.8 90.6 64.3l35.2 100.5c23.2 9.6 39.6 32.5 39.6 59.2V400v48c0 17.7-14.3 32-32 32H448c-17.7 0-32-14.3-32-32V400H96v48c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V400 256c0-26.7 16.4-49.6 39.6-59.2zM128 288a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm288 32a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"/></svg></a>
